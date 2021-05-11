25e6901c49c6b74a120baca1f0071ae84b062d69
Fernanda Leite <fernandacarolineleite360@gmail.com>
2021-05-10 21:22:57 -0300

    feat: add access control

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,12 @@
 import { useContext, useEffect } from "react"
 
+import { Can } from "../components/Can"
+
 import { AuthContext } from "../contexts/AuthContext"
+
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
+
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
@@ -14,7 +18,15 @@ export default function Dashboard() {
       .catch(err => console.log(err))
   }, [])
 
-  return (<h1>Dashboard: {user?.email}</h1>)
+  return (
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      <Can permissions={['metrics.list']} >
+        <div>Métricas</div>
+      </Can>
+    </>
+  )
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
